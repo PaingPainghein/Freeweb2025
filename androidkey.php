64e90a4ebac29e6055a96fbf7f9eaf89e53a4bff
PaingPainghein <125305378+PaingPainghein@users.noreply.github.com>
2025-02-02 04:11:48 +0700

    Update androidkey.php

--- androidkey.php
+++ androidkey.php
@@ -1,1 +1,116 @@
+<!DOCTYPE html>
+<html lang="my">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Android Free VPN Key 2025</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .card-title {
+            font-size: 1rem;
+        }
+        .title { color: black; }
+        .dtac { color: blue; }
+        .true { color: red; }
+        .ais { color: green; }
+    </style>
+</head>
+<body>
+    
+    
+    <div class="container">
+        <header class="my-4">
+         <h class="card-title">Android Free Key VPN  </p>       
+         </h>
+        </header>
+
+        <div class="row">
+            <!-- http Card -->
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9FrYOJm98tEq3-xGpDT5az6hCemnNHBPRqccawUwG2w&s" alt="Dtac Logo" class="card-img-center" style="max-height: 50px; width: auto;">
+    <p>Http injector Free key 👇</p>
+    
+     
+        <p ထည့်</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- natmod Card -->
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <img src="https://a.fsdn.com/allura/p/netmodhttp/icon?99c9b36557ba775b887edf3ace70a11913221a3aef9c700274ef45e088672583?&w=180" alt="True Logo" class="card-img-center" style="max-height: 30px; width: auto;">
+                   
+     <p>NetMod VPN Free Key 👇 </p>
+  
+        <p class="true"></p>
+          
+                              
+                    </div>
+                </div>
+            </div>
+            <!-- v2box Card -->
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <img src="https://img.utdstc.com/icon/8f7/f6d/8f7f6dba7f928494eb306adfe7c17d13910b600476f39edc935f0311b3973420:200" alt="Ais Logo" class="card-img-center" style="max-height: 50px; width: auto;">
+        
+         <p>V2Box Free key 👇</p>
+    
+     ထည့်
+     </p>
+     
+     </div>
+                </div>
+            </div>
+
+            <!-- armod Card -->
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <img src="https://image.winudf.com/v2/image1/Y29tLmFydHVubmVsNTdfaWNvbl8xNjQyMzE1NDAwXzAzNw/icon.webp?w=280&fakeurl=1&type=.webp" alt="True Logo" class="card-img-center" style="max-height: 30px; width: auto;">
+                   
+     <p>ArMod VPN Free Key 👇 </p>
+  
+        <p class="true"></p>
+          
+                              
+                    </div>
+                </div>
+            </div>
+            <!-- http Card -->
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <img src="https://image.winudf.com/v2/image1/eHl6LmVhc3lwcm8uaHR0cGN1c3RvbV9pY29uXzE3MDE1ODQ0NTFfMDc3/icon.webp?w=280&fakeurl=1&type=.webp" alt="Ais Logo" class="card-img-center" style="max-height: 50px; width: auto;">
+        
+         <p>Http custom VPN Free key 👇</p>
+    
+     ထည့်
+     </p>
+     
+    
+     
+        
+                            </div>
+                </div>
+            </div>
+        </div>
+
+        <footer class="text-center mt-4">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzTPLWKmO4toXLKXnfgJldeBb8afUEXutTpA&usqp=CAU" alt="Dtac Logo" class="card-img-center" style="max-height: 50px; width: auto;">
+            <p>&copy; PPH 2025 Android Free Key VPN</p>
+        </footer>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
 
